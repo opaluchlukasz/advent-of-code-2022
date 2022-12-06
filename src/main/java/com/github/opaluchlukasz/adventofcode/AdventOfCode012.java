62695b3cef04320d746916bfb2b38929b6fff426
lukasz.opaluch <lukasz.opaluch@ocado.com>
2022-12-06 17:49:28 +0100

    day 6

---
+++ src/main/java/com/github/opaluchlukasz/adventofcode/AdventOfCode012.java
@@ -0,0 +1,26 @@
+package com.github.opaluchlukasz.adventofcode;
+
+import org.eclipse.collections.impl.set.mutable.primitive.CharHashSet;
+
+import java.util.List;
+
+public class AdventOfCode012 {
+
+    private static final int MESSAGE_LENGTH = 14;
+
+    public static void main(String[] args) {
+        List<String> lines = FileUtils.read("input_006");
+        System.out.println(solve(lines.get(0)));
+    }
+
+    private static int solve(String line) {
+        for (int i = 0; i < line.length() - MESSAGE_LENGTH; i++) {
+            CharHashSet set = new CharHashSet();
+            set.addAll(line.substring(i, i  + MESSAGE_LENGTH).toCharArray());
+            if (set.size() == MESSAGE_LENGTH) {
+                return i + MESSAGE_LENGTH;
+            }
+        }
+        return 0;
+    }
+}
